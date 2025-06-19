7592fc024745a8c849d344a4ce09acef319319f2
Donggi Yang <xronace@gmail.com>
2025-06-19 10:13:04 +0900

    feat: Add files required for azure web app deployment

---
+++ MorningBriefingApp/streamlit.sh
@@ -0,0 +1,4 @@
+pip install streamlit authlib azure-ai-agents azure-identity python-dotenv google-auth google-api-python-client streamlit-js-eval requests caldav psycopg2-binary sqlalchemy
+
+
+python -m streamlit run Main.py --server.port 8000 --server.address 0.0.0.0
